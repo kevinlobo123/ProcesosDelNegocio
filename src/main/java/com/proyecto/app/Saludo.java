049f556eb5e1b3c4819edefb2ef0afc5822b0bf6
SCIS3-PC01\SCIS3-PC01 <prueba>
2022-03-16 18:57:47 -0500

    primer commit para subir los archivos iniciales del proyecto

---
+++ src/main/java/com/proyecto/app/Saludo.java
@@ -0,0 +1,19 @@
+package com.proyecto.app;
+
+public class Saludo {
+    private final long id;
+    private final String contenido;
+
+    public Saludo(long id, String contenido){
+        this.id = id;
+        this.contenido = contenido;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getContenido(){
+        return contenido;
+    }
+}
---
+++ src/main/java/com/proyecto/app/SaludoController.java
@@ -0,0 +1,18 @@
+package com.proyecto.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class SaludoController {
+    private static final String plantilla = "Hola, %s!";
+    private final AtomicLong contador = new AtomicLong();
+
+    @GetMapping("/saludo")
+    public Saludo verSaludo(@RequestParam(value = "nombre",defaultValue = "Mundo") String nombre){
+        return new Saludo(contador.incrementAndGet(), String.format(plantilla,nombre));
+    }
+}
