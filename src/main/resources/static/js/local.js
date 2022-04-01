b0ad51cbb5dc8a15206abe2b8e4332320763bdda
SCIS3-PC01\SCIS3-PC01 <prueba>
2022-03-31 20:03:22 -0500

    implementacion del front

---
+++ src/main/resources/static/js/local.js
@@ -0,0 +1,18 @@
+//funciones propias de la app
+function sendData(path){
+    var myForm = document.getElementById("myForm");
+    var formData = new FormData(myForm);
+    var jsonData = {};
+    for(var [k, v] of formData){//convertimos los datos a json
+        jsonData[k] = v;
+    }
+    const request = fetch(path, {
+        method: 'POST',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jsonData)
+    });
+    console.log(request)
+}
