f871720467c4a14be6c82cac8b1d9e8ddb3b2a48
SCIS3-PC01\SCIS3-PC01 <prueba>
2022-05-11 19:38:52 -0500

    agregamos modificar y visualizar tambien visualizamos el listado de usuarios dinamicamente

--- src/main/resources/static/js/local.js
+++ src/main/resources/static/js/local.js
@@ -32,11 +32,25 @@ function listar(){
     fetch("api/users",settings)
     .then(response => response.json())
     .then(function(data){
-        if(data.lenght>0){
+        
+        //if(data.lenght>0){
+            var usuarios = '';
             for(const usuario of data){
                 console.log(usuario.email)
+                usuarios += '<tr>'+
+                '<th scope="row">'+usuario.id+'</th>'+
+                '<td>'+usuario.firstName+'</td>'+
+                '<td>'+usuario.lastName+'</td>'+
+                '<td>'+usuario.email+'</td>'+
+                '<td>'+
+                  '<button type="button" class="btn btn-outline-danger"><i class="fa-solid fa-user-minus"></i></button>'+
+                  '<a href="modificar.html" class="btn btn-outline-warning"><i class="fa-solid fa-user-pen"></i></a>'+
+                  '<a href="visualizar.html" class="btn btn-outline-info"><i class="fa-solid fa-eye"></i></a>'+
+                '</td>'+
+              '</tr>';
             }
-        }
+            document.getElementById("listar").innerHTML = usuarios;
+        //}
     })
 }
 
